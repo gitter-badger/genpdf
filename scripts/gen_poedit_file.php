50518141603c0c6daeddb466fa65c5301ec06bef
romainbessuges <romainbessuges@gmail.com>
2013-03-14 09:25:50 +0100

    Compatibilité PHP 5.3

--- scripts/gen_poedit_file.php
+++ scripts/gen_poedit_file.php
@@ -1,21 +1,25 @@
 <?php
 
-$dirIterator   = new RecursiveDirectoryIterator(__DIR__ . '/../templates');
 $cacheFilename = __DIR__ . "/../cache/twig_gettext_keys.php";
-$pattern = '#\{% ?trans "([].*)"#';
+$pattern = '#\{% ?trans "(.*)"#';
 
 if (file_exists($cacheFilename)) {
     unlink($cacheFilename);
 }
 /** @var SplFileInfo $file */
-foreach (new RecursiveIteratorIterator($dirIterator, RecursiveIteratorIterator::LEAVES_ONLY) as $file) {
-    if ($file->isFile() && $file->getExtension() == 'twig') {
-        $content = file_get_contents($file->getPathname());
-        preg_match_all($pattern, $content, $matches);
-        $keys = array_unique($matches[1]);
-        array_walk($keys, function (&$key) {
-            $key = "_('$key');";
-        });
-        file_put_contents($cacheFilename, "<?php " . PHP_EOL . implode(PHP_EOL, $keys));
-    }
+
+$directory = new RecursiveDirectoryIterator(__DIR__ . '/../templates');
+$iterator = new RecursiveIteratorIterator($directory);
+$files = new RegexIterator($iterator, '/^.+\.twig$/i', RecursiveRegexIterator::GET_MATCH);
+
+foreach($files as $filename => $v){
+    $content = file_get_contents($filename);
+    preg_match_all($pattern, $content, $matches);
+    $keys = array_unique($matches[1]);
+    array_walk($keys, function (&$key) {
+        $key = "_('$key');";
+    });
+    file_put_contents($cacheFilename, "<?php " . PHP_EOL . implode(PHP_EOL, $keys));
 }
+
+
