08504224c4c48bef3f452b6b014cf0d80b9f3d7c
romainbessuges <romainbessuges@gmail.com>
2013-03-22 10:45:48 +0100

    trouvé !

--- lib/Exaprint/GenPDF/Resources/PrintboxSupplierInvoice.php
+++ lib/Exaprint/GenPDF/Resources/PrintboxSupplierInvoice.php
@@ -4,6 +4,12 @@ namespace Exaprint\GenPDF\Resources;
 
 class PrintboxSupplierInvoice extends SupplierInvoice
 {
+    public function getTemplateFilename()
+    {
+        return "resources/printbox/supplier-invoice.twig";
+    }
+
+
     public function getHeader()
     {
         return "header.printbox.html";
