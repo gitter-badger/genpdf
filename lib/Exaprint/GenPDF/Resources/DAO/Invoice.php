5de21868ce30a8451b86b6570d0717a8821d1188
Menencia <menencia@gmail.com>
2013-09-18 15:41:43 +0200

    Correction de la longueur du XML reçu.

--- lib/Exaprint/GenPDF/Resources/DAO/Invoice.php
+++ lib/Exaprint/GenPDF/Resources/DAO/Invoice.php
@@ -22,16 +22,28 @@ class Invoice
     public function getXML($IDFacture)
     {
         $db = DB::get();
-        $db->exec('SET QUOTED_IDENTIFIER ON');
-        $db->exec('SET ANSI_WARNINGS ON');
-        $db->exec('SET ANSI_PADDING ON');
+
         $db->exec('SET ANSI_NULLS ON');
+        $db->exec('SET ANSI_PADDING ON');
+        $db->exec('SET ANSI_WARNINGS ON');
+        $db->exec('SET ARITHABORT ON');
         $db->exec('SET CONCAT_NULL_YIELDS_NULL ON');
+        $db->exec('SET NUMERIC_ROUNDABORT OFF');
+        $db->exec('SET QUOTED_IDENTIFIER ON');
+
+        $query = "select LEN(CAST(dbo.f_XML_Facture($IDFacture) AS nvarchar(max)))";
+        if ($result = $db->query($query)) {
+            if (($length = $result->fetchColumn()) !== false) {
+                $txt = "SET TEXTSIZE " . ($length*2);
+                $db->exec($txt);
+            }
+        }
 
         $query = "select CAST(dbo.f_XML_Facture($IDFacture) AS nvarchar(max))";
         if ($result = $db->query($query)) {
             if (($xml = $result->fetchColumn()) !== false) {
                 $xml = str_replace("\r\n", "", $xml);
+
                 if ($simplexml = simplexml_load_string($xml)) {
                     return $simplexml;
                 }
