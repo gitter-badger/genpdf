79042a9f2868c40011251f6f4789d73d6812450e
Menencia <menencia@gmail.com>
2013-10-15 14:41:14 +0200

    Correction de la position du footer

--- public/index.php
+++ public/index.php
@@ -60,11 +60,11 @@ $app->get("/:name/:id.pdf", function ($name, $id) use ($app) {
         $wkhtml = new \RBM\Wkhtmltopdf\Wkhtmltopdf();
 
         $wkhtml->setHeaderHtml($_SERVER["SERVER_NAME"] . "/static/assets/" . $resource->getHeader());
-        $wkhtml->setMarginTop(32);
+        $wkhtml->setMarginTop(50);
         $wkhtml->setHeaderSpacing(5);
         $wkhtml->setFooterHtml($_SERVER["SERVER_NAME"] . "/static/assets/" . $resource->getFooter());
         $wkhtml->setFooterSpacing(5);
-        $wkhtml->setMarginBottom(32);
+        $wkhtml->setMarginBottom(45);
 
         $r = $wkhtml->run($_SERVER["SERVER_NAME"] . "/$name/$id.html", $filename);
 
