6a0612b62edf6848f2124b77aaf99076c0183458
Menencia <menencia@gmail.com>
2014-11-13 11:29:49 +0100

    Correction syntaxe SQL

--- docs/VUE_PDF_PLANCHE.sql
+++ docs/VUE_PDF_PLANCHE.sql
@@ -2,62 +2,62 @@ DROP VIEW dbo.VUE_PDF_PLANCHE;
 
 CREATE VIEW dbo.VUE_PDF_PLANCHE AS
   SELECT
-    p.IDPlanche                                              AS IDPlanche,
-    p.IDPlanchePrincipale                                    AS IDPlanchePrincipale,
-    p.IDAtelier                                              AS IDAtelier,
-    p.DateVisibleAtelier                                     AS DateVisibleAtelier,
-    p.Quantité                                               AS NbFeuilles,
-    dbo.IsZero(p.Largeur, pf.Largeur)                        AS Largeur,
-    dbo.IsZero(p.Longueur, pf.Longueur)                      AS Longueur,
-    p.DateExpeditionUrgente                                  AS ExpeSansFaconnage,
-    p.DateExpeditionUrgenteFaconnage                         AS ExpeAvecFaconnage,
-    dbo.f_dValeurOptionValeurPlanche(p.IDPlanche, 80, 1)     AS NbCouleursRecto,
-    dbo.f_dValeurOptionValeurPlanche(p.IDPlanche, 81, 1)     AS NbCouleursVerso,
-    dbo.f_nIDProduitOptionValeurPlanche(p.IDPlanche, 75, 1)  AS Support,
-    dbo.f_nIDProduitOptionValeurPlanche(p.IDPlanche, 78, 1)  AS PelliculageRecto,
-    dbo.f_nIDProduitOptionValeurPlanche(p.IDPlanche, 79, 1)  AS PelliculageVerso,
-    dbo.f_nIDProduitOptionValeurPlanche(p.IDPlanche, 80, 1)  AS ImpressionRecto,
-    dbo.f_nIDProduitOptionValeurPlanche(p.IDPlanche, 81, 1)  AS ImpressionVerso,
-    dbo.f_nIDProduitOptionValeurPlanche(p.IDPlanche, 85, 1)  AS VernisRecto,
-    dbo.f_nIDProduitOptionValeurPlanche(p.IDPlanche, 86, 1)  AS VernisVerso,
-    dbo.f_nIDProduitOptionValeurPlanche(p.IDPlanche, 87, 1)  AS VernisSelectifRecto,
-    dbo.f_nIDProduitOptionValeurPlanche(p.IDPlanche, 88, 1)  AS VernisSelectifVerso,
-    dbo.f_nIDProduitOptionValeurPlanche(p.IDPlanche, 89, 1)  AS Decoupe,
-    dbo.f_nIDProduitOptionValeurPlanche(p.IDPlanche, 90, 1)  AS Perforation,
-    dbo.f_nIDProduitOptionValeurPlanche(p.IDPlanche, 93, 1)  AS DorureRecto,
-    dbo.f_nIDProduitOptionValeurPlanche(p.IDPlanche, 96, 1)  AS TypeImpression,
-    dbo.f_nIDProduitOptionValeurPlanche(p.IDPlanche, 98, 1)  AS Rainage,
-    dbo.f_nIDProduitOptionValeurPlanche(p.IDPlanche, 99, 1)  AS Predecoupe,
-    dbo.f_nIDProduitOptionValeurPlanche(p.IDPlanche, 100, 1) AS EncreAGratter,
-    dbo.f_nIDProduitOptionValeurPlanche(p.IDPlanche, 104, 1) AS Pliage,
-    dbo.f_nIDProduitOptionValeurPlanche(p.IDPlanche, 105, 1) AS DecoupeALaForme,
-    dbo.f_nIDProduitOptionValeurPlanche(p.IDPlanche, 147, 1) AS Encollage,
-    p.AvecDecoupe,
-    p.AvecDorure,
-    p.AvecEncollage,
-    p.AvecEncre,
-    p.AvecPerforation,
-    p.AvecRainage,
-    p.AvecPliage,
-    p.Bascule,
-    p.NbCoinsRonds,
-    p.ObservationsTechnique,
-    p.IDProduitPlancheFormat,
-    p.EstAR,
-    p.EstSousTraitance,
-    p.IDProduitActiviteProduction,
-    p_soustraitance.IDPlanche                                AS IDPlancheSousTraitance,
-    a_soustraitance.Nom                                      AS NomAtelierSousTraitance,
-    a_principale.Nom                                         AS NomAtelierPlanchePrincipale,
-    monteur.NomUtilisateur                                   AS NomMonteur,
-    monteur.PrenomUtilisateur                                AS PrenomMonteur,
-    monteur.EmailUtilisateur                                 AS EmailMonteur,
-    p.DateAjout,
-    p1.Nom                                                   AS NomAtelier,
-    (SELECT
-    SUM(Cout)
-     FROM TBL_COMMANDE_PR
-     WHERE IDPlanche = p.IDPlanche)                          AS CoutPlanche
+      p.IDPlanche                                              AS IDPlanche
+    , p.IDPlanchePrincipale                                    AS IDPlanchePrincipale
+    , p.IDAtelier                                              AS IDAtelier
+    , p.DateVisibleAtelier                                     AS DateVisibleAtelier
+    , p.Quantité                                               AS NbFeuilles
+    , dbo.IsZero(p.Largeur, pf.Largeur)                        AS Largeur
+    , dbo.IsZero(p.Longueur, pf.Longueur)                      AS Longueur
+    , p.DateExpeditionUrgente                                  AS ExpeSansFaconnage
+    , p.DateExpeditionUrgenteFaconnage                         AS ExpeAvecFaconnage
+    , dbo.f_dValeurOptionValeurPlanche(p.IDPlanche, 80, 1)     AS NbCouleursRecto
+    , dbo.f_dValeurOptionValeurPlanche(p.IDPlanche, 81, 1)     AS NbCouleursVerso
+    , dbo.f_nIDProduitOptionValeurPlanche(p.IDPlanche, 75, 1)  AS Support
+    , dbo.f_nIDProduitOptionValeurPlanche(p.IDPlanche, 78, 1)  AS PelliculageRecto
+    , dbo.f_nIDProduitOptionValeurPlanche(p.IDPlanche, 79, 1)  AS PelliculageVerso
+    , dbo.f_nIDProduitOptionValeurPlanche(p.IDPlanche, 80, 1)  AS ImpressionRecto
+    , dbo.f_nIDProduitOptionValeurPlanche(p.IDPlanche, 81, 1)  AS ImpressionVerso
+    , dbo.f_nIDProduitOptionValeurPlanche(p.IDPlanche, 85, 1)  AS VernisRecto
+    , dbo.f_nIDProduitOptionValeurPlanche(p.IDPlanche, 86, 1)  AS VernisVerso
+    , dbo.f_nIDProduitOptionValeurPlanche(p.IDPlanche, 87, 1)  AS VernisSelectifRecto
+    , dbo.f_nIDProduitOptionValeurPlanche(p.IDPlanche, 88, 1)  AS VernisSelectifVerso
+    , dbo.f_nIDProduitOptionValeurPlanche(p.IDPlanche, 89, 1)  AS Decoupe
+    , dbo.f_nIDProduitOptionValeurPlanche(p.IDPlanche, 90, 1)  AS Perforation
+    , dbo.f_nIDProduitOptionValeurPlanche(p.IDPlanche, 93, 1)  AS DorureRecto
+    , dbo.f_nIDProduitOptionValeurPlanche(p.IDPlanche, 96, 1)  AS TypeImpression
+    , dbo.f_nIDProduitOptionValeurPlanche(p.IDPlanche, 98, 1)  AS Rainage
+    , dbo.f_nIDProduitOptionValeurPlanche(p.IDPlanche, 99, 1)  AS Predecoupe
+    , dbo.f_nIDProduitOptionValeurPlanche(p.IDPlanche, 100, 1) AS EncreAGratter
+    , dbo.f_nIDProduitOptionValeurPlanche(p.IDPlanche, 104, 1) AS Pliage
+    , dbo.f_nIDProduitOptionValeurPlanche(p.IDPlanche, 105, 1) AS DecoupeALaForme
+    , dbo.f_nIDProduitOptionValeurPlanche(p.IDPlanche, 147, 1) AS Encollage
+    , p.AvecDecoupe
+    , p.AvecDorure
+    , p.AvecEncollage
+    , p.AvecEncre
+    , p.AvecPerforation
+    , p.AvecRainage
+    , p.AvecPliage
+    , p.Bascule
+    , p.NbCoinsRonds
+    , p.ObservationsTechnique
+    , p.IDProduitPlancheFormat
+    , p.EstAR
+    , p.EstSousTraitance
+    , p.IDProduitActiviteProduction
+    , p_soustraitance.IDPlanche                                AS IDPlancheSousTraitance
+    , a_soustraitance.Nom                                      AS NomAtelierSousTraitance
+    , a_principale.Nom                                         AS NomAtelierPlanchePrincipale
+    , monteur.NomUtilisateur                                   AS NomMonteur
+    , monteur.PrenomUtilisateur                                AS PrenomMonteur
+    , monteur.EmailUtilisateur                                 AS EmailMonteur
+    , p.DateAjout
+    , p1.Nom                                                   AS NomAtelier
+    , (SELECT
+      SUM(Cout)
+       FROM TBL_COMMANDE_PR
+       WHERE IDPlanche = p.IDPlanche)                          AS CoutPlanche
   FROM TBL_PLANCHE p
     LEFT JOIN TBL_PLANCHE_FORMAT pf ON pf.IDPlancheFormat = p.IDProduitPlancheFormat
     LEFT JOIN TBL_PLANCHE p_soustraitance ON p_soustraitance.IDPlanchePrincipale = p.IDPlanche
