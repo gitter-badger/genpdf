c3703bb5c876752dfe7c8f68b31ff7d377bffbde
Menencia <menencia@gmail.com>
2014-12-03 10:26:34 +0100

    Ajout des coins ronds pour la découpe numérique

--- lib/Exaprint/GenPDF/FicheDeFabrication/Amalgame/Cellule/CoinsRonds.php
+++ lib/Exaprint/GenPDF/FicheDeFabrication/Amalgame/Cellule/CoinsRonds.php
@@ -15,48 +15,69 @@ use Exaprint\TCPDF\Color;
 use Exaprint\TCPDF\Dimensions;
 use Exaprint\TCPDF\FillColor;
 use Exaprint\TCPDF\Font;
+use Exaprint\TCPDF\ImageInContainer;
 use Exaprint\TCPDF\Position;
 use Exaprint\TCPDF\Rect;
 use Exaprint\TCPDF\RoundedRect;
-use Exaprint\TCPDF\Text;
 use Exaprint\TCPDF\TextColor;
 
-class CoinsRonds implements ICellule {
+class CoinsRonds implements ICellule
+{
 
     public function draw(Position $position, \TCPDF $pdf, $cellSize, array $commande)
     {
-        switch($commande['DecoupeALaForme']){
-            case 522:
-                $cell = new Cell();
-                $cell->position = $position;
-                $cell->fill = true;
-                $cell->fillColor = new FillColor(Color::black());
-                $cell->width = $cellSize;
-                $cell->height = $cellSize;
-                $cell->border = true;
-                $cell->draw($pdf);
+        if ($commande['DecoupeALaForme'] != null) {
+            switch ($commande['DecoupeALaForme']) {
+                case 522:
+                    $cell            = new Cell();
+                    $cell->position  = $position;
+                    $cell->fill      = true;
+                    $cell->fillColor = new FillColor(Color::black());
+                    $cell->width     = $cellSize;
+                    $cell->height    = $cellSize;
+                    $cell->border    = true;
+                    $cell->draw($pdf);
 
-                $rr = new RoundedRect();
-                $rr->radius = 2;
-                $rr->position = $position->add(new Position(0.5, 0.5));
-                $rr->dimensions = new Dimensions($cellSize-1, $cellSize-1);
-                $rr->fillColor = new FillColor(Color::white());
-                $rr->style = Rect::STYLE_FILL;
-                $rr->draw($pdf);
+                    $rr             = new RoundedRect();
+                    $rr->radius     = 2;
+                    $rr->position   = $position->add(new Position(0.5, 0.5));
+                    $rr->dimensions = new Dimensions($cellSize - 1, $cellSize - 1);
+                    $rr->fillColor  = new FillColor(Color::white());
+                    $rr->style      = Rect::STYLE_FILL;
+                    $rr->draw($pdf);
 
-                $txt = new Cell();
-                $txt->position = $position;
-                $txt->width = $cellSize;
-                $txt->height = $cellSize;
-                $txt->align = Cell::ALIGN_CENTER;
-                $txt->vAlign = Cell::VALIGN_CENTER;
-                $txt->font = new Font('bagc-bold', 18, new TextColor(Color::black()));
-                $txt->fill = false;
-                $txt->text = 'CR';
-                $txt->draw($pdf);
-                break;
-            default:
-                Helper::drawEmptyCell($position, $pdf, $cellSize);
+                    $txt           = new Cell();
+                    $txt->position = $position;
+                    $txt->width    = $cellSize;
+                    $txt->height   = $cellSize;
+                    $txt->align    = Cell::ALIGN_CENTER;
+                    $txt->vAlign   = Cell::VALIGN_CENTER;
+                    $txt->font     = new Font('bagc-bold', 18, new TextColor(Color::black()));
+                    $txt->fill     = false;
+                    $txt->text     = 'CR';
+                    $txt->draw($pdf);
+                    break;
+                default:
+                    Helper::drawEmptyCell($position, $pdf, $cellSize);
+            }
+        }
+        if ($commande['DecoupeALaFormeNumerique'] != null) {
+
+            $file = '../assets/coins-ronds/CR_';
+            $file .= $commande['HasDecoupeNumeriqueHG'];
+            $file .= $commande['HasDecoupeNumeriqueHD'];
+            $file .= $commande['HasDecoupeNumeriqueBD'];
+            $file .= $commande['HasDecoupeNumeriqueBG'];
+            $file .= '.png';
+
+            $image = new ImageInContainer(
+                $file,
+                new Dimensions(71, 71),
+                new Dimensions($cellSize, $cellSize),
+                $position
+            );
+
+            $image->draw($pdf);
         }
     }
 
