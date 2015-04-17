d13ebadb213cd7b62a480b91c4af9ceeff4cc086
Menencia <menencia@gmail.com>
2015-04-17 18:00:43 +0200

    Exaprod est considéré comme un template dérivant de Negoce

--- lib/Exaprint/GenPDF/FicheDeFabrication/Factory.php
+++ lib/Exaprint/GenPDF/FicheDeFabrication/Factory.php
@@ -10,6 +10,7 @@ namespace Exaprint\GenPDF\FicheDeFabrication;
 
 
 use Exaprint\GenPDF\FicheDeFabrication\Amalgame\Amalgame;
+use Exaprint\GenPDF\FicheDeFabrication\Negoce\Exaprod;
 use Exaprint\GenPDF\FicheDeFabrication\Negoce\Negoce;
 
 class Factory
@@ -17,7 +18,7 @@ class Factory
 
     public static function createFicheDeFabrication($planche)
     {
-        if(!$planche){
+        if (!$planche) {
             throw new \Exception('Planche is null');
         }
         switch ($planche['IDProduitActiviteProduction']) {
@@ -26,8 +27,15 @@ class Factory
             case 4:
             case 7:
                 return new Amalgame($planche);
+                break;
             case 3:
-                return new Negoce($planche);
+                $case = 1;
+                if ($case == 1) {
+                    return new Negoce($planche);
+                } else if ($case == 2) {
+                    return new Exaprod($planche);
+                }
+                break;
             default:
                 var_dump($planche);
                 return null;
